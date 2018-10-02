e986253824c25f61c2c95b007fe72558bceda2c6
Brooke Meleah <brooke.meleah@fluxfederation.com>
2018-10-02 22:03:14 +1300

    hardcoded some smal task cards or whatever

--- reactcompanion/Task.js
+++ reactcompanion/Task.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Button, Alert, Image, Icon, View, Text, StyleSheet, Platform, StatusBar } from 'react-native';
+import { Button, Alert, Image, Icon, View, Text, StyleSheet, Platform, StatusBar, FlatList } from 'react-native';
 import { StackNavigator, DrawerItems, SafeAreaView, createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import {Container, Content, Header, Body, Title, Card, CardItem } from 'native-base';
 import LinearGradient from "react-native-linear-gradient";
-import {Container, Content, Header, Body, Title} from 'native-base';
-
 import CustomHeader from './CustomHeader';
+
+const list = [
+  {
+    name: 'task one',
+    subtitle: 'subtitle one'
+  },
+  {
+    name: 'task two',
+    subtitle: 'subtitle two'
+  },
+];
 
 export default class Task extends React.Component {
   static navigationOptions = {
@@ -43,6 +53,26 @@ render() {
       <CustomHeader title={this.state.title} link= {() => this.props.navigation.openDrawer()} description= {this.state.description} />
         <Content>
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Card>
+            <CardItem>
+                <Text>task one</Text>
+            </CardItem>
+        </Card>
+        <Card>
+            <CardItem>
+                <Text>task two</Text>
+            </CardItem>
+        </Card>
+        <Card>
+            <CardItem>
+                <Text>task three</Text>
+            </CardItem>
+        </Card>
+        <Card>
+            <CardItem>
+                <Text>task four</Text>
+            </CardItem>
+        </Card>
           <Button
             onPress={() => this.props.navigation.goBack()}
             title="Go back home"
@@ -67,4 +97,8 @@ const styles = StyleSheet.create({
     width: 24,
     height: 24,
   },
+  taskText: {
+    fontSize: 18,
+    color: '#000'
+  }
 });
